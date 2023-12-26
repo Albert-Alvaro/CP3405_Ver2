7a3c01868d6d9ef298b8b0d39240272e1fd71816
albert_alvaro <albertvaro01@gmail.com>
2023-12-26 11:33:20 +0800

    start the project and create database models

---
+++ questions/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+admin.site.register(models.MultiChoiceQuestion)
+admin.site.register(models.ShortQuestion)
+admin.site.register(models.EssayQuestion)
+admin.site.register(models.EssayResponse)
+admin.site.register(models.ShortResponse)
+admin.site.register(models.Choices)
---
+++ questions/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+class Choices(models.Model):
+    choice = models.CharField(max_length=5000)
+
+class MultiChoiceQuestion(models.Model):
+    question = models.CharField(max_length=10000)
+    required = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    choices = models.ManyToManyField(Choices, related_name="choices")
+
+    def __str__(self):
+        return self.question
+
+class ShortQuestion(models.Model):
+    question = models.CharField(max_length=10000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
+
+class EssayQuestion(models.Model):
+    question = models.CharField(max_length=10000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
+
+class EssayResponse(models.Model):
+    question = models.ForeignKey(EssayQuestion, null=False, on_delete=models.CASCADE)
+    body = models.TextField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ShortResponse(models.Model):
+    question = models.ForeignKey(ShortQuestion, null=False, on_delete=models.CASCADE)
+    body = models.TextField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
