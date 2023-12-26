751406c3cb5da55532f7fead9c033e9ab98573dc
albert_alvaro <albertvaro01@gmail.com>
2023-12-26 11:58:15 +0800

    updated the files

--- questions/views.py
+++ questions/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
+    shortQuestions = ShortQuestion.objects.all().order_by('created_at')
+    multiChoiceQuestions = MultiChoiceQuestion.objects.all().order_by('created_at')
+    essayQuestions = EssayQuestion.objects.all().order_by('created_at')
     context = {
-        'title' : 'world'
+        'shortQuestions' : shortQuestions,
+        'essayQuestions' : essayQuestions,
+        'multiChoiceQuestions' : multiChoiceQuestions,
     }
     return render(request, 'index.html', context)
+
+def questionPage(request, id):
+    return None
